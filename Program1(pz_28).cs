65f504408a163fc399b3929d932e7d8930448d21
Benzomraz <112751181+Benzomraz@users.noreply.github.com>
2023-05-11 06:31:16 +0500

    Add files via upload

---
+++ Program1(pz_28).cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class Otshiot
+    {
+        // Определение делегата для события OnSchiot
+        public delegate void CounterEventHandler(int count);
+        public event CounterEventHandler OnSchiot;
+
+        public void Schiot()
+        {
+            // Генерация последовательности чисел от 1 до 1000
+            for (int i = 1; i <= 1000; i++)
+            {
+                // Вызов события OnSchiot с текущим значением счетчика
+                OnSchiot?.Invoke(i);
+
+                // Задержка на 0,1 секунды
+                Thread.Sleep(45);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание объекта
+            Otshiot counter = new Otshiot();
+
+            // Добавление обработчика события OnSchiot для вывода каждого значения отсчета на экран
+            counter.OnSchiot += count =>
+            {
+                Console.WriteLine(count);
+            };
+
+            // Добавление обработчика события OnSchiot
+            counter.OnSchiot += count =>
+            {
+                // Проверка значения счетчика и вывод сообщения при достижении значения 200
+                if (count == 200)
+                {
+                    Console.WriteLine("Вы дошли до 200!");
+                }
+            };
+
+            // Добавление обработчика события OnSchiot
+            counter.OnSchiot += count =>
+            {
+                // Проверка значения счетчика и вывод сообщения при достижении значения 800
+                if (count == 800)
+                {
+                    Console.WriteLine("Вы дошли до 800!");
+                }
+            };
+
+            // Запуск метода Schiot
+            counter.Schiot();
+        }
+    }
+}
---
+++ Program2(pz_28).cs
@@ -0,0 +1,97 @@
+﻿
+using System;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class Car
+    {
+        public int Speed { get; private set; }
+        public void Accelerate()
+        {
+            if (Speed < 200)
+                Speed++; // Увеличиваем скорость на 1 км/ч
+        }
+    }
+
+    class Patrol
+    {
+        private bool detentionModeActivated = false;
+        private bool speedWarning100 = false;
+        private bool speedWarning150 = false;
+        private int maxSpeedCount = 0;
+        public void CheckSpeed(Car car)
+        {
+            if (car.Speed > 120 && !detentionModeActivated)
+            {
+                Console.WriteLine("Активация режима задержания!");
+                detentionModeActivated = true;
+            }
+            else if (car.Speed == 100 && !speedWarning100)
+            {
+                Console.WriteLine("Предупреждение! Пожалуйста, снизьте скорость!");
+                speedWarning100 = true;
+            }
+            else if (car.Speed == 150 && !speedWarning150)
+            {
+                Console.WriteLine("Предупреждение! Пожалуйста, снизьте скорость!");
+                speedWarning150 = true;
+            }
+
+            if (car.Speed == 200)
+                maxSpeedCount++;
+
+            if (maxSpeedCount == 20)
+            {
+                Console.WriteLine("Вы оторвались от копов!");
+                Environment.Exit(0);
+            }
+        }
+    }
+
+    class Transmission
+    {
+        private bool activated = false;
+        public void CheckSpeed(Car car)
+        {
+            if ((car.Speed == 80 || car.Speed == 135) && !activated)
+            {
+                Console.WriteLine("Переключение передачи!");
+                activated = true;
+            }
+            else if (activated)
+            {
+                for (int i = 0; i < 5; i++)
+                    Console.WriteLine(car.Speed - i - 1);
+                activated = false;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Patrol patrol = new Patrol();
+            Transmission transmission = new Transmission();
+
+            Action accelerateAndCheckSpeed = () =>
+            {
+                car.Accelerate();
+                patrol.CheckSpeed(car);
+                transmission.CheckSpeed(car);
+            };
+
+            for (int i = 0; i < 250; i++)
+            {
+                accelerateAndCheckSpeed();
+                Console.WriteLine($"Current speed: {car.Speed}");
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
+
+
+
